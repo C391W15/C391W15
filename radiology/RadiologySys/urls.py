6a722b7c4460701b45803a2ce01a5166b96320fd
Cody Ingram <CodyIngram@CodyIngramsMBP.ed.shawcable.net>
2015-04-03 20:17:55 -0600

    Added help menu

--- radiology/RadiologySys/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from RadiologySys import views
-
-urlpatterns = patterns('', 
-	url(r'^home/', views.index, name='index'),
-	#url(r'^login/', views.user_login, name='login'),
-)
